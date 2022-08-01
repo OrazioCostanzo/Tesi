8366c18303c03b6ec1991dd787a615e8b17e0c10
Orazio Costanzo <orazio.costanzo20@gmail.com>
2022-08-01 17:34:14 +0200

    Date: 8/1/2022. change 1.3. Insert: Suffix.java,         SuffixArray.java,         SuffixArrayBuilder.java. refactored the code. Fatto il refactoring dei metodi della classe Suffix.

---
+++ Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Suffix"},{"p":"<Unnamed>","l":"SuffixArray"},{"p":"<Unnamed>","l":"SuffixArrayBuilder"}];updateSearchResults();
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        char[] gen = {'A','B','C','D'};
+        int loop = 100000;
+        for(int i = 0; i < loop; i++){
+            sb.append (gen[(int) (Math.random() * gen.length)]) ;
+        }
+        SuffixArray s = SuffixArrayBuilder.buildSuffixArray("ba.,,&nan          \nanana");
+        s.printSA();
+
+    }
+}
